4429811103e5d6387086ac4fd006129687869049
Gabriel Enrico <gabrielenrico0123@gmail.com>
2025-05-23 20:50:06 -0300

    feat: adicionar modelo de domínio de gerenciamento de pedidos

---
+++ src/models/order_system.py
@@ -0,0 +1,6 @@
+class OrderSystem:
+    def __init__(self):
+        self._users = [] # Lista de Usuários
+        self._products = [] # Lista de Produtos
+        self._orders = [] # Lista de Pedidos
+        self._customers = []  # Lista de Clientes
