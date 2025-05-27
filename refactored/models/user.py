967d55acc876da4e1f69f7af147e3c67bf3563bb
victuribdev <victuribdev@gmail.com>
2025-05-26 21:21:07 -0300

    feat(models): restaura modelos User, Customer, Product e Order para uso nos serviços e testes

---
+++ refactored/models/user.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self, username: str, password: str, user_type: str):
+        self.username = username
+        self.password = password
+        self.type = user_type 
